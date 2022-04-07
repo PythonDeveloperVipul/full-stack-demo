289aff5a20f4f6ad268a2dafe53ef27dac179f89
PythonDeveloperVipul <98796100+PythonDeveloperVipul@users.noreply.github.com>
2022-04-07 10:18:26 +0530

    Update settings.py

--- CarProject/settings.py
+++ CarProject/settings.py
@@ -127,6 +127,6 @@ AUTH_USER_MODEL = 'user.User'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'vipul.tagline@gmail.com'
-EMAIL_HOST_PASSWORD = 'tagline@123'
+EMAIL_HOST_USER = ["Email"]
+EMAIL_HOST_PASSWORD = ["Password"]
 EMAIL_USE_TLS = True
